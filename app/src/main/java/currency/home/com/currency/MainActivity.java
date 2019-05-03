210c05668306f5c30ac5dce27691e783f3b63d31
Yang <yangdengkai@yangdengkaide-MacBook-Pro.local>
2019-05-03 20:38:37 +0800

    HW2

--- app/src/main/java/currency/home/com/currency/MainActivity.java
+++ app/src/main/java/currency/home/com/currency/MainActivity.java
@@ -1,13 +1,55 @@
 package currency.home.com.currency;
 
+import android.content.DialogInterface;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText ntd;
+    private Button bnt_go;
+    private double NTD,USD;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        findViews();
+
+    }
+
+    private void findViews() {
+        ntd = findViewById(R.id.ntd);
+        bnt_go = findViewById(R.id.btn_go);
+        bnt_go.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if("".equals(ntd.getText().toString())){
+                    new AlertDialog.Builder(MainActivity.this)
+                            .setTitle("Problem")
+                            .setMessage("Please Enter Your NTD Amount!!!")
+                            .setPositiveButton("OK",null)
+                            .show();
+                }else{
+                    NTD = Double.parseDouble(ntd.getText().toString());
+                    USD = NTD / 30.9;
+                    new AlertDialog.Builder(MainActivity.this)
+                            .setTitle("Result")
+                            .setMessage("USD is" + USD)
+                            .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                                @Override
+                                public void onClick(DialogInterface dialogInterface, int i) {
+                                    ntd.setText("");
+                                }
+                            })
+                            .show();
+                }
+            }
+        });
     }
 }
